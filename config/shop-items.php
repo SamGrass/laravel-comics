a73d78eb5b38b5cb07fb74b5ca06b536dda5f86c
Samgrass <samuelegrassetti01@gmail.com>
2024-09-11 19:47:07 +0200

    add configs to use in page

---
+++ config/shop-items.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    [
+        "image" => "buy-comics-digital-comics.png",
+        "text" => "DIGITAL COMICS"
+    ],
+    [
+        "image" => "buy-comics-merchandise.png",
+        "text" => "DC MERCHANDISE"
+    ],
+    [
+        "image" => "buy-comics-shop-locator.png",
+        "text" => "SUBSCRIPTION"
+    ],
+    [
+        "image" => "buy-comics-subscriptions.png",
+        "text" => "COMIC SHOP LOCATOR"
+    ],
+    [
+        "image" => "buy-dc-power-visa.svg",
+        "text" => "DC POWER VISA"
+    ],
+];
---
+++ config/socials.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    ["social" => "footer-facebook.png"],
+    ["social" => "footer-periscope.png"],
+    ["social" => "footer-pinterest.png"],
+    ["social" => "footer-twitter.png"],
+    ["social" => "footer-youtube.png"]
+];
