9fa87381d78b3602bf7c389bc6a6339df40dfd15
Samgrass <samuelegrassetti01@gmail.com>
2024-09-11 18:48:03 +0200

    add static header from previous project

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,34 @@
+<div>
+    <header>
+        <div class="container">
+
+            <!-- img sx -->
+            <div class="image-container">
+                <img src="../assets/dc-logo.png" alt="...">
+            </div>
+            <!-- nav dx -->
+            <nav>
+                <ul>
+                    <li>
+                        <a class="" :href="#">Home</a>
+                    </li>
+                    <li>
+                        <a class="" :href="#">Home</a>
+                    </li>
+                    <li>
+                        <a class="" :href="#">Home</a>
+                    </li>
+                    <li>
+                        <a class="" :href="#">Home</a>
+                    </li>
+                    <li>
+                        <a class="" :href="#">Home</a>
+                    </li>
+
+
+                </ul>
+            </nav>
+        </div>
+    </header>
+</div>
+<div class="jumbotron"></div>
