4b12d00966f8ceb041774fc629481c04b7239165
Samgrass <samuelegrassetti01@gmail.com>
2024-09-11 18:50:22 +0200

    add static footer from previous project

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,53 @@
-<footer class="my-5 text-center">
-    <p>by UdU</p>
+<footer class="">
+    <!-- link acquisti -->
+    <div class="shop">
+        <div class="container shop-container">
+            <ul>
+                <li>
+                    <a href="#!">
+                        <div class="image-container">
+                            <img src="" alt="item.text">
+                        </div>
+                        <div class="shop-text">
+                            testo
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- link footer -->
+    <div class="useful-links">
+        <div class="container flex-column">
+            <div class="link-list">
+                <h3>titolo</h3>
+                <ul>
+                    <li>
+                        <a href="#!">links</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- socials -->
+    <div class="socials">
+        <div class="container">
+            <!-- sign up sx-->
+            <a href="#!" class="btn-sign-up">
+                SIGN-UP NOW!
+            </a>
+
+            <!-- socials dx-->
+            <div class="social-icons">
+                <h3>FOLLOW US</h3>
+                <ul>
+                    <li>
+                        <a href="#!">
+                            <img src="" alt="social">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 </footer>
